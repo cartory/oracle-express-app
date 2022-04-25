f8f895057fa21c6db97906d5de9fbd43a25106ad
pedro.caricari <pedro.caricari@bg.com.bo>
2022-04-24 20:43:49 -0400

    update index.ts

--- index.ts
+++ index.ts
@@ -1,10 +1,8 @@
-import morgan from "morgan"
-
 import app from "./src/app"
 
 const PORT = process.env.PORT
 
-app.use(morgan("dev")).listen(PORT, () => {
+app.listen(PORT, () => {
 	console.log(new Date())
 	console.log(`Server runnning on http://localhost:${PORT}/`)
 })
